1610511935df953f8362e218c297b46e6e572dda
Doc1998 <37333134+Doc1998@users.noreply.github.com>
2022-07-14 12:12:15 +0100

    updates

--- src/app/homeheader/homeheader.component.ts
+++ src/app/homeheader/homeheader.component.ts
@@ -1,12 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-homeheader',
-  templateUrl: './homeheader.component.html',
-  styleUrls: ['./homeheader.component.css'],
+  selector: "app-homeheader",
+  templateUrl: "./homeheader.component.html",
+  styleUrls: ["./homeheader.component.css"]
 })
 export class HomeheaderComponent implements OnInit {
+  colorHeader: boolean = false;
+  showDropDown: boolean = false;
+  scrWidth = window.innerWidth;
+  phoneView: boolean = true;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    console.log(this.scrWidth);
+    if (this.scrWidth > 1024) {
+      this.phoneView = false;
+    }
+  }
+
+  @HostListener("window:scroll", ["$event"]) onScroll() {
+    if (!this.phoneView) {
+      if (window.scrollY > 100) {
+        this.colorHeader = true;
+      } else {
+        this.colorHeader = false;
+      }
+    }
+  }
+  @HostListener("window:resize")
+  getScreenSize() {
+    this.scrWidth = window.innerWidth;
+    if (this.scrWidth > 1024) {
+      this.phoneView = false;
+    } else {
+      this.phoneView = true;
+    }
+    console.log(this.scrWidth);
+  }
+  dropDownMenu(target: HTMLElement) {
+    this.showDropDown = !this.showDropDown;
+    if (target.classList.contains("clicked")) {
+      console.log("yes");
+      target.classList.remove("clicked");
+    } else {
+      console.log("no");
+      target.classList.add("clicked");
+    }
+  }
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: "smooth", block: "center" });
+  }
 }
